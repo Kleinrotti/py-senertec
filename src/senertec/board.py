e2bb330ce436801f180f8f06c7e7c37066bd0f6e
Kleinrotti <kleinrotti@gmail.com>
2022-02-05 16:55:09 +0100

    new function to search for a datapoint in boards

--- src/senertec/board.py
+++ src/senertec/board.py
@@ -16,6 +16,12 @@ class board(object):
         """Get the full datapoint string."""
         return self.boardName + "." + self.datapoints[index]
 
+    def getDatapointByName(self, name: str):
+        """Get a datapoint by its name. Not case sensitive."""
+        for point in self.datapoints:
+            if name.lower() in point.friendlyName.lower():
+                return point
+
     def getFullDataPointIds(self):
         """Get all datapoints."""
         lst = []
