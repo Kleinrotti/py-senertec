cc474a8d0168833b9b775d0e0e4602a574fe3065
Kleinrotti <kleinrotti@gmail.com>
2022-04-20 15:44:31 +0200

    updated tests

--- tests/data_test.py
+++ tests/data_test.py
@@ -10,25 +10,34 @@ from senertec.client import senertec
 class TestConnection(TestCase):
     def __init__(self, methodName: str = ...) -> None:
         super().__init__(methodName=methodName)
+
+    def setUp(self):
         file = open(os.getcwd() + "\\productGroups.json")
         supportedItems = json.load(file)
         file.close()
         self.senertec = senertec(supportedItems,
-                                 os.environ['SENERTECUSER'], os.environ['SENERTECPW'],level=logging.DEBUG)
-
-    def test_brennstoffzellenChart(self):
-        self.senertec.login()
-        self.senertec.init()
-        serial = self.senertec.getUnits()
-        self.senertec.connectUnit(serial[0].serial)
-        chart = self.senertec.getChart("ChartChpActivityFCEnduser")
-        self.senertec.logout()
-        self.assertTrue(chart != None)
-
-    def test_request(self):
+                                 os.environ['SENERTECUSER'], os.environ['SENERTECPW'], level=logging.DEBUG)
         self.senertec.login()
         self.senertec.init()
         self.senertec.messagecallback = self.output
+
+    def tearDown(self):
+        self.senertec.logout()
+
+    def test_brennstoffzellenChart(self):
+        serial = self.senertec.getUnits()
+        self.senertec.connectUnit(serial[0].serial)
+        chart = self.senertec.getChart("ChartChpActivityFCEnduser")
+
+        self.assertTrue(chart != None)
+
+    def test_errors(self):
+        serial = self.senertec.getUnits()
+        self.senertec.connectUnit(serial[0].serial)
+        errors = self.senertec.getErrors()
+        self.assertIsNotNone(errors)
+
+    def test_request(self):
         serial = self.senertec.getUnits()
         self.senertec.connectUnit(serial[0].serial)
         for points in self.senertec.boards:
@@ -36,7 +45,6 @@ class TestConnection(TestCase):
             response = self.senertec.request(l)
             self.assertTrue(response)
         sleep(3)
-        self.senertec.logout()
 
     def output(self, value: canipValue):
         print("Source: " + value.sourceDatapoint + "\nBoard: " +
