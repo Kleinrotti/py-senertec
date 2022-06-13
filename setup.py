b444b208ecda8e5e9a1b38502e74333220d80142
Kleinrotti <kleinrotti@gmail.com>
2022-06-13 14:39:57 +0200

    updated version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="py-senertec",
-    version="0.2.1",
+    version="0.2.2",
     author="Kleinrotti",
     author_email="",
     package_dir={"": "src"},
