62d2aed257224926ff0f83b4f7ff14cf42aebc7e
Kleinrotti <kleinrotti@gmail.com>
2022-08-31 21:33:01 +0200

    added teamcity support to tests

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,11 +1,14 @@
 import json
 import logging
 import os
-from unittest import TestCase
+import unittest
+from teamcity import is_running_under_teamcity
+from teamcity.unittestpy import TeamcityTestRunner
 from senertec.canipValue import canipValue
 from senertec.client import senertec
 
-class TestBase(TestCase):
+
+class TestBase(unittest.TestCase):
     def __init__(self, methodName: str = ...) -> None:
         super().__init__(methodName=methodName)
 
@@ -24,3 +27,10 @@ class TestBase(TestCase):
 
     def output(self, value: canipValue):
         pass
+
+if __name__ == '__main__':
+    if is_running_under_teamcity():
+        runner = TeamcityTestRunner()
+    else:
+        runner = unittest.TextTestRunner()
+    unittest.main(testRunner=runner)
