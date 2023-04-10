bd3cca0bd8246b4a83a4ac0300cd1f0d67476682
Kleinrotti <kleinrotti@gmail.com>
2023-04-10 21:38:48 +0200

    updated release workflow

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="py-senertec",
-    version="0.3.0",
+    version="0.0.0",
     author="Kleinrotti",
     author_email="",
     package_dir={"": "src"},
