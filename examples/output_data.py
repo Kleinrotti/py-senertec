dea8aa4f8b3ad9fa7f9cf44d092328dba31cdfea
Kleinrotti <kleinrotti@gmail.com>
2023-04-15 18:08:04 +0200

    updated example script

--- examples/output_data.py
+++ examples/output_data.py
@@ -3,12 +3,13 @@
 # This example outputs all values to the console window with a list of errors of your energy unit.
 #
 #
-from senertec.lang import lang
 from senertec.client import senertec
 from senertec.canipValue import canipValue
 import json
 from time import sleep
 import os
+
+result = []
 
 
 def start():
@@ -17,49 +18,88 @@ def start():
     file = open(os.getcwd() + "\\datapointFilter.json")
     filter = json.load(file)
     file.close()
-    global senertec
-    # set your username and password here
-    senertec = senertec(filter, lang.German)
+    # create a new senertec object with filter
+    senertecClient = senertec(filter)
+    global result
+    totalDataPoints = 0
+    timeoutCounter = 0
 
     # to get all possible datapoints you can specify None
-    # senertec = senertec(None,"username", "password", lang.German)
+    # senertec = senertec(None)
 
     # set your callback function where received data should go to
-    senertec.messagecallback = output
+    senertecClient.messagecallback = output
 
-    # login to dachsportal2
-    if senertec.login("username", "password") is False:
+    # login to dachsportal2 with email and password
+    if senertecClient.login("username", "password") is False:
         return
-    # if login was successful you can/should initialize the platform.
-    if senertec.init() is False:
+    # if login was successful you need to initialize the platform.
+    if senertecClient.init() is False:
         return
     # get all energy units/heating systems from your account.
-    units = senertec.getUnits()
+    units = senertecClient.getUnits()
     # connect to first unit
-    if senertec.connectUnit(units[0].serial) is False:
+    if senertecClient.connectUnit(units[0].serial) is False:
         return
     print(f"Connected to unit: {units[0].model} with serial: {units[0].serial}")
     # get errors to output them below
-    errors = senertec.getErrors()
-    # request datapoints. In this case all points from all boards.
-    for points in senertec.boards:
-        ids = points.getFullDataPointIds()
-        senertec.request(ids)
+    errors = senertecClient.getErrors()
 
-    # if no datapointfilter was set above, you need to increase the sleep time a lot
-    # because there are over 400 datapoints in most cases which take some time to be received
-    sleep(5)
-    senertec.logout()
+    # request all datapoints.
+    for board in senertecClient.boards:
+        ids = board.getFullDataPointIds()
+        senertecClient.request(ids)
+        totalDataPoints += len(board.datapoints)
+
+    # if you want specific datapoints you can loop through the boards to find it and request it then
+    #for board in senertecClient.boards:
+    #    datapoint = board.getFullDatapointIdByName("MM011")
+    #    if(datapoint):
+    #        senertecClient.request(datapoint)
+    #        totalDataPoints += 1
+    #        break
+
+    
+    # loop wait 2 seconds and checks if all points get received
+    while (len(result) < totalDataPoints):
+        # break after 60sec timeout
+        if timeoutCounter > 30:
+            break
+        sleep(2)
+        timeoutCounter += 1
+    # logout after all datapoints get received or timeout was reached
+    senertecClient.logout()
+
+    # print all values
+    for value in result:
+        print("Source: " + value.sourceDatapoint + "\nName: " + value.friendlyDataName + "\nValue: " +
+              value.dataValue.__str__() + value.dataUnit + "\n")
+
+    #print total count of received values  
+    print(f"Received {len(result)} values.")
+
+    # print all errors
     print("\n-----Device errors-----")
     for e in errors:
         if (e.code != ""):
             print(
                 f"\nError: {e.code} in Board: {e.boardName} \nCategorie: {e.errorCategory} \nMessage: {e.errorTranslation}")
 
+
 # this is the callback function to get the requested data from the websocket connection.
 def output(value: canipValue):
-    print("Source: " + value.sourceDatapoint + "\nName: " + value.friendlyDataName + "\nValue: " +
-          value.dataValue.__str__() + value.dataUnit + "\n")
+    global result
+    temp = next(
+        (
+            x
+            for x in result
+            if x.sourceDatapoint == value.sourceDatapoint
+        ),
+        None,
+    )
+    # sometimes we get values twice, so we filter it
+    if not temp:
+        result.append(value)
 
 
 start()
